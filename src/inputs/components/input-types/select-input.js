4dee396ba4bd0bc5d0ad41a9978f8b6c9aef46bd
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2016-01-11 14:55:34 -0800

    inputs: add "select" input type

---
+++ src/inputs/components/input-types/select-input.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Select from "react-select";
+class SelectInput extends Component {
+    handleChange(chosenOption) {
+        this.props.inputUpdate(chosenOption.value);
+    }
+
+    render() {
+        let { value, options } = this.props.input;
+
+        return (
+            <div className="form-group">
+                <Select
+                    name="form-field-name"
+                    value={value}
+                    options={options.items}
+                    clearable={false}
+                    onChange={this.handleChange.bind(this)}
+                />
+            </div>
+        );
+    }
+}
+
+export default SelectInput;
