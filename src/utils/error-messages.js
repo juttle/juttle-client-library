b7206f51d477befdcb94e1836e0d50d616bafeeb
Matt Nibecker <mnibecker@jut.io>
2016-02-12 15:24:37 -0800

    Revert "Revert "Juttle view errors 2""
    This reverts commit 9a683906857fd8075e79564d12a28dda13d901c6.

--- src/utils/error-messages.js
+++ src/utils/error-messages.js
@@ -1,5 +1,5 @@
 export default {
-    'JOB-START-ERROR': 'Event \'job_start\' not received as first message from socket. Problem.',
-    'JUTTLE-VIEW-PARAMS-INVALID': 'The following views have invalid parameters: {{info.viewNames}}',
+    'JOB-START-ERROR': 'Event \"job_start\" not received as first message from socket. Problem.',
+    'JUTTLE-VIEW-PARAMS-INVALID': '\"{{info.juttleView}}\" has invalid parameters: {{info.errorMessages}}',
     'JUTTLE-VIEW-UNKNOWN-ERROR': 'Unknown/Unsupported view type: {{info.type}}'
 };
