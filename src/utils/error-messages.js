380f0e1e80092b439394e68e2b1950b4265fb270
Matt Nibecker <mnibecker@jut.io>
2016-02-12 15:14:40 -0800

    Revert "Handle Juttle-Viz invalid parameter errors"
    This reverts commit a802298994e0d176ffcdc7157c756aa16241135f.

--- src/utils/error-messages.js
+++
@@ -1,5 +0,0 @@
-export default {
-    'JOB-START-ERROR': 'Event \'job_start\' not received as first message from socket. Problem.',
-    'JUTTLE-VIEW-PARAMS-INVALID': 'The following views have invalid parameters: {{info.viewNames}}',
-    'JUTTLE-VIEW-UNKNOWN-ERROR': 'Unknown/Unsupported view type: {{info.type}}'
-};
